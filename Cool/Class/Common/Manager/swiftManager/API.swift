8fecbfe10cd38e9c63c673ca1908508176d340c3
yangwen <ouy15818731124@gmail.com>
2022-11-07 14:41:51 +0800

    1、添加多张图片上传

--- Cool/Class/Common/Manager/swiftManager/API.swift
+++ Cool/Class/Common/Manager/swiftManager/API.swift
@@ -13,7 +13,8 @@ enum API{
     case login(parameters:[String:Any])
     case register(parameters:[String:Any])
     case balencelist(parameters:[String:Any])
-    case uploadHeadImage(parameters: [String:Any], imageDate:Data)
+    case uploadHeadImage(parameters: [String:Any], imageData:Data)
+    case uploadImage(parameters: [String:Any], dataAry:Data)
 }
 
 extension API: TargetType{
@@ -35,7 +36,9 @@ extension API: TargetType{
             return "/index.php?m=Api&c=User&a=user_cash_out"
         case .balencelist:
             return balence_API // 收益明细
-        case .uploadHeadImage(parameters: _, imageDate: _):
+        case .uploadHeadImage(parameters: _, imageData: _):
+            return "/file/user/upload.jhtml"
+        case .uploadImage(parameters: _, dataAry: _):
             return "/file/user/upload.jhtml"
       }
     }
@@ -72,11 +75,29 @@ extension API: TargetType{
         case let .balencelist(parameters):
             return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
             
-        case .uploadHeadImage(parameters: let parameters, imageDate: let imgDate):
-            let formData = MultipartFormData(provider: .data(imgDate), name: "file",
+        case .uploadHeadImage(parameters: let parameters, imageData: let imgData):
+            let formData = MultipartFormData(provider: .data(imgData), name: "file",
                                              fileName: "hangge.png", mimeType: "image/png")
             return .uploadCompositeMultipart([formData], urlParameters: parameters)
-
+            
+        case .uploadImage(parameters: let parameters, dataAry: let imgDatas):
+            
+            let formDataAry:NSMutableArray = NSMutableArray()
+            for (index,image) in imgDatas.enumerated() {
+                //图片转成Data
+                let data:Data = (image as! UIImage).jpegData(compressionQuality: 0.9)!
+               //根据当前时间设置图片上传时候的名字
+                let date:Date = Date()
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd-HH:mm:ss"
+                var dateStr:String = formatter.string(from: date as Date)
+                //别忘记这里给名字加上图片的后缀哦
+                dateStr = dateStr.appendingFormat("-%i.png", index)
+               
+                let formData = MultipartFormData(provider: .data(data), name: "file", fileName: dateStr, mimeType: "image/jpeg")
+                formDataAry.add(formData)
+           }
+            return .uploadCompositeMultipart(formDataAry as! [MultipartFormData], urlParameters: parameters )
         }
     }
     
